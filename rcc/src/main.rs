631d04a24db386b49a32b0ee756bf6afb5530eff
Kaku <105181329+kakucodes@users.noreply.github.com>
2023-01-27 20:21:53 +0700

    added rcc

--- rcc/src/main.rs
+++ rcc/src/main.rs
@@ -13,15 +13,5 @@
 mod args;
 
 fn main() {
-    // print::run();
-    // vars::run();
-    // types::run();
-    // tuples::run();
-    // array::run();
-    // loops::run();
-    // fns::run();
-    // pointer_ref::run();
-    // structs::run();
-    // enums::run();
     args::run()
 }
